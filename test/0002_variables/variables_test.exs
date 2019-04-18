425472b37f3f04e160bf2b536e4325401916d24a
Tyler Hawkins <tyler@getdivvy.com>
2019-04-18 10:27:52 -0600

    got rid of warnings

--- test/0002_variables/variables_test.exs
+++ test/0002_variables/variables_test.exs
@@ -3,32 +3,24 @@ defmodule VariablesTest do
 
   describe "Test variables" do
     test "setting a number" do
-      number = nil
-
       # set number to the right value
       number = 1
       assert number == 1
     end
 
     test "setting a string" do
-      string = nil
-
       # set string to the right value
       string = "a string"
       assert string == "a string"
     end
 
     test "setting a map" do
-      map = nil
-
       # set map to the right value
       map = %{test: "test"}
       assert map == %{test: "test"}
     end
 
     test "Pull apart tuple" do
-      first = nil
-      second = nil
       tuple = {:first, 2}
 
       # pull apart tuple to set both variables
@@ -38,8 +30,6 @@ defmodule VariablesTest do
     end
 
     test "Pull apart map" do
-      first = nil
-      second = nil
       map = %{item_a: "first", item_b: "second"}
 
       # pull apart map to set both variables
