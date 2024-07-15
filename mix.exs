4eb1569c7320a91e1f2150b516e821b56d7ca956
Tyler Hawkins <tylerhawkins55@gmail.com>
2024-07-15 11:24:58 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirTraining.MixProject do
     [
       app: :elixir_training,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: ["lib", "test/support"],
       deps: deps()
