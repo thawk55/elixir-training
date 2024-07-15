fbae229581a8928f30b6347a7a7a915ff638d396
thawk55 <tylerhawkins55@gmail.com>
2024-07-15 12:12:20 -0600

    bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,8 +5,9 @@ defmodule ElixirTraining.MixProject do
     [
       app: :elixir_training,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: ["lib", "test/support"],
       deps: deps()
     ]
   end
@@ -21,7 +22,9 @@ defmodule ElixirTraining.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 0.8"}
+      {:ecto_sql, "~> 3.0"},
+      {:mix_test_watch, "~> 0.8"},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 end
