1b14ecd332a2007666a13c7aa05a9870a7e02ab8
Tyler Hawkins <tylerhawkins55@gmail.com>
2024-07-15 11:53:41 -0600

    Update types.ex

--- lib/0001_types/types.ex
+++ lib/0001_types/types.ex
@@ -13,27 +13,35 @@ defmodule Types do
   end
 
   def string() do
+
   end
 
   def integer() do
+
   end
 
   def float() do
+
   end
 
   def boolean() do
+
   end
 
   def list() do
+
   end
 
   def tuple() do
+
   end
 
   def map() do
+
   end
 
   # Struct name defined in the file is Types
   def struct() do
+
   end
 end
